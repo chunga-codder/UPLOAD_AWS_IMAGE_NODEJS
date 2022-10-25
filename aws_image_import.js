258e2f254b6e0ae10c0bbd39f27fab93da692870
Clinton abumbi <clintonchunga@gmail.com>
2022-10-25 16:04:44 -0700

    uploading the image and using the .promise() at the end so upload is complete

--- aws_image_import.js
+++ aws_image_import.js
@@ -1,4 +1,4 @@
-import AWS from 'aws-sdk';
+import AWS, { S3 } from 'aws-sdk';
 
 // creating an aws s3 object instance
 
@@ -11,3 +11,10 @@ S3 = new AWS.S3({
 const imageURL = 'https://url-to-my-image'
 const res = await fetch(imageURL)
 const blob = await res.arrayBuffer();
+
+//now making a call to s3.upload() and calling the .promise to wait for the image to upload
+const uploadedImage = await S3.upload({
+    Bucket : process.env.AWS_S3_BUCKET_NAME,
+    Key : req.files[0].originalFilename,
+    Body: blob,
+}).promise();
